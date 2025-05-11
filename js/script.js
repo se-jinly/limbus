6a9245ce0c47ce44b80876150ed041e22c4e51a2
se-jinly <sejinahn421@gmail.com>
2025-05-11 13:05:12 +0900

    test
    test

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("footer > span").click(function(){
+        $(".popup").show();
+    })
+    $(".popup > button").click(function(){
+        $(".popup").close();
+    })
+
+    $(".").click
+})
