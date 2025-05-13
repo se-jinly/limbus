521ac66af4d601428b9e929f78e58b7da95c1b78
se-jinly <sejinahn421@gmail.com>
2025-05-13 13:50:24 +0900

    init project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
 
-    $("footer > span").click(function(){
-        $(".popup").show();
-    })
-    $(".popup > button").click(function(){
-        $(".popup").close();
-    })
+    
 
-    $(".").click
 })
